266b023e614d0dc2dd31d31bacb8bdcc3114a840
Archanaredy <89598132+Archanaredy@users.noreply.github.com>
2022-10-08 12:21:39 -0500

    Added drawable files with images and xml files

--- app/src/main/java/com/example/gymmanagementapp/DescriptionPage.java
+++ app/src/main/java/com/example/gymmanagementapp/DescriptionPage.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
+
 
 public class DescriptionPage extends AppCompatActivity {
     TextView name, description;
