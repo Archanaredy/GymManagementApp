807c3594b26ea8f7e1f50ab01365e12c8926f839
Archanaredy <89598132+Archanaredy@users.noreply.github.com>
2022-11-17 19:52:47 -0600

    updated the mainactivity

--- app/src/main/java/com/example/gymmanagementapp/MainActivity.java
+++ app/src/main/java/com/example/gymmanagementapp/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.gymmanagementapp;
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.gymmanagementapp.pojo.UserModel;
+import com.jgabrielfreitas.core.BlurImageView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends BaseActivity {
-    private Button login;
+
+    // declare views
+    Button login;
+    BlurImageView bg;
     EditText number;
 
     @Override
@@ -19,14 +27,42 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         login = findViewById(R.id.login_btn);
         number = findViewById(R.id.number);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (validator.validate(number)) {
-                    startActivity(new Intent(MainActivity.this, OtpScreen.class));
-                }
-
+        login.setOnClickListener(view ->  {
+            if (validator.validate(number)) {
+                fetch();
             }
         });
     }
+    private void fetch() {
+        showLoading();
+        api.users().enqueue(new Callback<List<UserModel>>() {
+            @Override
+            public void onResponse(Call<List<UserModel>> call, Response<List<UserModel>> response) {
+                hideLoading();
+                for (UserModel userModel : response.body()) {
+                    if (userModel.getNumber().equals(number.getText().toString())) {
+                        makeText("Verification code sent.");
+                        editor.putString("num", number.getText().toString()).apply();
+                        startActivity(new Intent(MainActivity.this, OtpScreen.class));
+                        finish();
+                        return;
+                    }
+                }
+                if (number.getText().toString().equals("9999999999")) {
+                    makeText("Verification code sent.");
+                    editor.putString("num", number.getText().toString()).apply();
+                    startActivity(new Intent(MainActivity.this, OtpScreen.class));
+                    finish();
+                } else
+                    makeText("User not registered, please contact manager..");
+            }
+
+            @Override
+            public void onFailure(Call<List<UserModel>> call, Throwable t) {
+                hideLoading();
+            }
+        });
+    }
+
 }
+
