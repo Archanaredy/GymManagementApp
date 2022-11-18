305032313f1f88a9b827f4ef08c4b39b794a12ff
Archanaredy <89598132+Archanaredy@users.noreply.github.com>
2022-11-17 20:10:01 -0600

    updated OtpScreen

--- app/src/main/java/com/example/gymmanagementapp/OtpScreen.java
+++ app/src/main/java/com/example/gymmanagementapp/OtpScreen.java
@@ -1,15 +1,18 @@
 package com.example.gymmanagementapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.gymmanagementapp.pojo.UserModel;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class OtpScreen extends BaseActivity {
-    private Button login;
+    Button login;
     EditText number;
 
     @Override
@@ -18,14 +21,32 @@ public class OtpScreen extends BaseActivity {
         setContentView(R.layout.activity_otp_screen);
         login = findViewById(R.id.login_btn);
         number = findViewById(R.id.number);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (validator.validate(number)) {
-                    startActivity(new Intent(OtpScreen.this, AdminDashBoard.class));
+        login.setOnClickListener(view -> {
+            if (validator.validate(number, "Please enter a valid code", 4)) {
+                makeText("Verification successful");
+                if (preferences.getString("num", "").equals("9999999999")) {
+                    startActivity(new Intent(OtpScreen.this, AdminDashBoard.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    finish();
+                } else {
+                    fetchUser();
                 }
-
             }
         });
     }
+    private void fetchUser() {
+        api.getUser(preferences.getString("num", "")).enqueue(new Callback<UserModel>() {
+            @Override
+            public void onResponse(Call<UserModel> call, Response<UserModel> response) {
+                editor.putString("id", response.body().getId()).apply();
+                startActivity(new Intent(OtpScreen.this, UserDashBoard.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<UserModel> call, Throwable t) {
+                makeText(t.getMessage());
+            }
+        });
+    }
+
 }
